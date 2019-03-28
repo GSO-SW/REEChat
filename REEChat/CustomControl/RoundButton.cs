24c10f525cd14947c718f188b472591e69da35ea
René Evertz <rene.evertz.98@gmail.com>
2019-03-28 13:44:31 +0100

    fixed wrong dll (CustomControll) path added dll (CustopmControll) to project path

---
+++ REEChat/CustomControl/RoundButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CustomControl
+{
+	public class RoundButton : Button
+	{
+		protected override void OnPaint(PaintEventArgs pevent)
+		{
+			GraphicsPath path = new GraphicsPath();
+			path.FillMode = FillMode.Winding;
+			path.AddEllipse(0, 0, Height, Height);
+			path.AddEllipse(Width - Height, 0, Height, Height);			
+			path.AddRectangle(new Rectangle(Height / 2, 0, Width - Height, Height));
+			this.Region = new Region(path);
+			base.OnPaint(pevent);
+		}
+	}
+}
